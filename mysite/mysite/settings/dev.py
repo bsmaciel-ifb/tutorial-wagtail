bf0ef531e54f2b2384ab656091f31f9078b6243a
bsmaciel <contatosbrunomaciel@gmail.com>
2025-09-29 16:58:20 -0300

    Start do projeto no Wagtail

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-%%p78z(k(v17!&lq&-oyyub7-u*umi^2qtc)qmk2^_+p_n!4%8"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
